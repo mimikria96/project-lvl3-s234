dc2b088e144405efd0007a8be5ce9787375f6d32
mimikria96 <chemiakina7493@mail.ru>
2018-05-01 12:18:45 +0300

    fix first step

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const test = () => {
-  const element = document.createElement('div');
-  element.innerHTML = 'No!';
-  return element;
-};
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import isURL from 'validator/lib/isURL';
 
-document.body.appendChild(test());
+function isValid() {
+  if (!isURL(this.value)) {
+    this.style = 'box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 1px 8px rgba(255,0,0,.6)';
+    return;
+  }
+  this.style = 'box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)';
+}
+// const addButton = document.querySelector('#mainAddButton');
+// const divNewUrlArea = document.querySelector('#addNewUrlArea');
+const inputNewUrl = document.querySelector('#inputNewUrl');
+
+inputNewUrl.addEventListener('click', isValid);
