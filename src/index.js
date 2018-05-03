e4877c5d83092ec4266a1cc78ceed0ac27fe3826
mimikria96 <chemiakina7493@mail.ru>
2018-05-03 19:22:59 +0300

    change htmlgen

--- src/index.js
+++ src/index.js
@@ -3,55 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import isURL from 'validator/lib/isURL';
 import axios from 'axios';
 import $ from 'jquery';
+import * as modal from './modal';
+
 
 const urlList = new Set();
+const showedNews = new Set();
 const addButton = document.getElementById('mainAddButton');
-// const divNewUrlArea = document.getElementById('addNewUrlArea');
+const updateButton = document.getElementById('mainUpdateButton');
 const inputNewUrl = document.getElementById('inputNewUrl');
 const domParser = new DOMParser();
 
-const generateModal = (i) => {
-  const parent = document.createElement('div');
-  parent.classList.add('modal');
-  parent.classList.add('fade');
-  parent.id = `ModalCenter${i}`;
-  parent.setAttribute('tabindex', '-1');
-  parent.setAttribute('role', 'dialog');
-  parent.setAttribute('aria-hidden', 'true');
-  const modaldialog = document.createElement('div');
-  modaldialog.classList.add('modal-dialog');
-  modaldialog.setAttribute('role', 'document');
-  const modalcontent = document.createElement('div');
-  modalcontent.classList.add('modal-content');
-  const modalheader = document.createElement('div');
-  modalheader.classList.add('modal-header');
-  const headerButton = document.createElement('button');
-  headerButton.type = 'button';
-  headerButton.classList.add('close');
-  headerButton.setAttribute('data-dismiss', 'modal');
-  headerButton.setAttribute('area-label', 'Close');
-  const span = document.createElement('span');
-  span.setAttribute('aria-hidden', 'true');
-  span.innerHTML = '&times;';
-  const modalbody = document.createElement('div');
-  modalbody.classList.add('modal-body');
-  headerButton.append(span);
-  modalheader.append(headerButton);
-  modalcontent.append(modalheader);
-  modalcontent.append(modalbody);
-  modaldialog.append(modalcontent);
-  parent.append(modaldialog);
-  return parent;
-};
-const generateModalButton = (i) => {
-  const button = document.createElement('button');
-  button.classList.add('btn');
-  button.classList.add('btn-primary');
-  button.setAttribute('data-toggle', 'modal');
-  button.setAttribute('data-target', `#ModalCenter${i}`);
-  button.type = 'button';
-  button.textContent = 'открыть описание';
-  return button;
+const generateLi = (item) => {
+  const liForLinks = document.createElement('li');
+  const a = document.createElement('a');
+  a.innerHTML = item.firstElementChild.innerHTML;
+  a.href = item.firstElementChild.nextElementSibling.nextElementSibling.innerHTML;
+  liForLinks.append(a);
+  const genId = a.href.slice(a.href.length - 3);
+  liForLinks.append(document.createElement('br'));
+  liForLinks.append(modal.genModal(genId));
+  liForLinks.append(modal.genButton(genId));
+  $(`#ModalCenter${genId}`).on('show.bs.modal', (e) => {
+    const modalBody = e.relatedTarget.nextElementSibling.querySelector('.modal-body');
+    modalBody.textContent = item.getElementsByTagName('description')[0].innerHTML;
+  });
+  const quid = item.getElementsByTagName('guid')[0].innerHTML;
+  showedNews.add(quid);
+  return liForLinks;
 };
 const generateHtml = (element) => {
   const parent = document.getElementById('urlList');
@@ -65,19 +43,7 @@ const generateHtml = (element) => {
   const items = channel.getElementsByTagName('item');
   const itemsParent = document.getElementById('itemsList');
   for (let i = 0; i < items.length; i += 1) {
-    const liForLinks = document.createElement('li');
-    const a = document.createElement('a');
-    a.innerHTML = items[i].firstElementChild.innerHTML;
-    a.href = items[i].firstElementChild.nextElementSibling.nextElementSibling.innerHTML;
-    liForLinks.append(a);
-    const genId = a.href.slice(a.href.length - 2);
-    liForLinks.append(generateModalButton(genId));
-    liForLinks.append(generateModal(genId));
-    itemsParent.prepend(liForLinks);
-    $(`#ModalCenter${genId}`).on('show.bs.modal', (e) => {
-      const modalBody = e.relatedTarget.nextElementSibling.querySelector('.modal-body');
-      modalBody.textContent = items[i].getElementsByTagName('description')[0].innerHTML;
-    });
+    itemsParent.prepend(generateLi(items[i]));
   }
 };
 
@@ -105,6 +71,16 @@ function addNewUrl() {
     loadContent(inputNewUrl.value);
     inputNewUrl.value = '';
     inputNewUrl.dataset.status = 'none';
+  }
+}
+function addNewItems(document) {
+  const items = document.getElementsByTagName('item');
+  const itemsParent = document.getElementById('urlList');
+  for (let i = 0; i < items.length; i += 1) {
+    const quid = items[i].getElementsByTagName('guid')[0].innerHTML;
+    if (!showedNews.has(quid)) {
+      itemsParent.prepend(generateLi(items[i]));
+    }
   }
 }
 
@@ -138,7 +114,15 @@ function handlePaste(e) {
     this.style = '';
   }
 }
+function update() {
+  console.log('a');
+  Promise.all(Array.from(urlList).map(getxmlFromUrl))
+    .then(xmls => xmls.map(el => domParser.parseFromString(el, 'application/xml')))
+    .then(documents => documents.map(addNewItems))
+    .catch(e => console.log(e));
+}
 
 inputNewUrl.addEventListener('keyup', isValid);
 inputNewUrl.addEventListener('paste', handlePaste);
 addButton.addEventListener('click', addNewUrl);
+updateButton.addEventListener('click', update);
